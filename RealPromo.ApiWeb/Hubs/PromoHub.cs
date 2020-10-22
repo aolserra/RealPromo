ff72b0081ee61188b62c9eb579a687010dfcf104
aolserra <aolserra@gmail.com>
2020-10-22 20:26:08 -0300

    Projeto Iníciado - Foi criada a interface Web e o Hub que será responsavel pelo disparo das mensagens

---
+++ RealPromo.ApiWeb/Hubs/PromoHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealPromo.ApiWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealPromo.ApiWeb.Hubs
+{
+    public class PromoHub : Hub
+    {
+        /*
+         * Client - JS/C#/Java
+         * RPC
+         * Client -> Hub
+         * Hub -> Client
+         */
+        public async Task CadastrarPromocao(Promocao promocao)
+        {
+            /*
+             * Banco
+             * Queue/Scheduler.......
+             * Notificar o usuário (SignalR).
+             */
+
+            await Clients.Caller.SendAsync("CadastradoSucesso");
+            await Clients.Others.SendAsync("ReceberPromocao", promocao);
+
+        }
+    }
+}
